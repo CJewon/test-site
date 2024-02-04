6f3d0b741ca725beeb4d967306c07e4d8fc61294
CJewon <112820744+CJewon@users.noreply.github.com>
2024-02-04 22:30:43 +0900

    asd

---
+++ router-test/src/App.js
@@ -0,0 +1,17 @@
+import './reset.css'
+import './App.css';
+
+import {Route, Routes, Link, BrowserRouter} from 'react-router-dom'
+
+import Main from './Component/Main';
+import ExplainSite from './Component/ExplainSite';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
